f7a52c339e2d2a26dcf3a9b9cd24509fed5b3646
beatriz <beatriz.nnds@gmail.com>
2022-04-11 14:01:03 -0300

    git commit -m  Versão finallll

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-let qtdCartas = Number(prompt("Com quantas cartas você quer jogar? De 4 a 14."));
+let qtdCartas;
 let arrayCartas = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif'];
 let carta = document.querySelectorAll(".cartas");
 let cartaSelecionada1;
 let cartaSelecionada2;
-let qtdCliques;
+let qtdCliques = 0;
+let parzinho = 0;
 
-while (qtdCartas % 2 !== 0 || qtdCartas < 4 || qtdCartas > 14 ) { 
+function pedirCartas () {
     qtdCartas = Number(prompt("Selecione um número par de 4 a 14."));
+    while (qtdCartas % 2 !== 0 || qtdCartas < 4 || qtdCartas > 14) { 
+        qtdCartas = Number(prompt("Selecione um número par de 4 a 14."));
+    }    
+    abrirCartas();
 }
+
 
 function abrirCartas () {
     arrayCartas.sort(comparador);
@@ -41,24 +47,43 @@ function mudarEscondido (carta) {
 }
 
 function viradaCarta() {
-    mudarEscondido(cartaSelecionada1);
-    mudarEscondido(cartaSelecionada2);    
+    cartaSelecionada1.querySelector(".frente").classList.toggle("escondido");
+    cartaSelecionada1.querySelector(".verso").classList.toggle("escondido");
+    cartaSelecionada2.querySelector(".frente").classList.toggle("escondido");
+    cartaSelecionada2.querySelector(".verso").classList.toggle("escondido");
+    cartaSelecionada1 = null;
+    cartaSelecionada2 = null; 
 }
 
 function cartasIguais () {
     cartaSelecionada1.classList.add("virada");
+    cartaSelecionada1.classList.remove("movimento-carta");
     cartaSelecionada2.classList.add("virada");    
+    cartaSelecionada2.classList.remove("movimento-carta");
+    cartaSelecionada1 = null;
+    cartaSelecionada2 = null; 
 }
 
-function selecionarCarta (carta) {
+function checarIguais () {
+    if (cartaSelecionada1.innerHTML === cartaSelecionada2.innerHTML) {
+        cartasIguais();
+        parzinho++;        
+    } else {
+        setTimeout(viradaCarta, 1000);
+    }
+    finalJogo();
+}   
+
+function selecionarCarta(carta) {
     if (!carta.classList.contains("virada")) {
-        mudarEscondido (carta);
-        carta.classList.toggle("movimento-carta");  
+        mudarEscondido(carta);
+        qtdCliques++;
+        carta.classList.toggle("movimento-carta");
         if (cartaSelecionada1) {
-            cartaSelecionada2 = carta
-            if (cartaSelecionada1.innerHTML === cartaSelecionada2.innerHTML) {
-                cartasIguais();
-        }   } else {
+            cartaSelecionada2 = carta;
+            setTimeout(checarIguais, 1000);
+         
+        } else {
             cartaSelecionada1 = carta;
         }
     }
@@ -66,6 +91,15 @@ function selecionarCarta (carta) {
 
 
 function finalJogo () {
-    //if (cada carta selecionada tiver par na tela) {
-        //alert (`Você ganhou em ${qtdCliques} jogadas!`)
+    if (parzinho === qtdCartas/2) {
+        alert(`Você ganhou em ${qtdCliques} jogadas!`);
+        let resposta = prompt("Você gostaria de jogar novamente?");
+        if (resposta === "sim") {
+         qtdCartas = null;   
+         document.querySelector(".jogo").innerHTML = ''; 
+         setTimeout(pedirCartas(), 1000);
+        }
     }
+}
+
+pedirCartas();
